b82dac941be7d3d7510cca6e5ef940544a83f61e
Ram <ram@cloudflare.com>
2011-10-20 00:41:43 -0700

    Complete re-write
    No longer uses the experimental webRequest API, added a content script that fetches the same page after load asynchronously and checks the response headers and passes a message to the bg process to set the icon

--- source/claire.js
+++ source/claire.js
@@ -5,32 +5,20 @@
 */
 
 
+// intercept the message passed by the content script and set the right icon
 
-// on request complete inspect the server header
 
-// if the cloudflare-nginx header is found, pass message to bg page
-
-chrome.tabs.onCreated.addListener(function(tab) {
-    chrome.experimental.webRequest.onCompleted.addListener(function(details) {
-        var on_cf = false;
-        var response_headers = details.responseHeaders;
-        details.responseHeaders.forEach(function(header) {
-            if (header.name == "Server" && header.value == "cloudflare-nginx") {
-                on_cf = true;
-                console.log("on CF");
-            }
-        });
-        
-        if (on_cf) {
-            chrome.pageAction.setIcon({tabId: tab.id, path: "images/orange_cloud.png"});
-            chrome.pageAction.show(tab.id);
-            console.log("set on CF icon");
-        } else {
-            chrome.pageAction.setIcon({tabId: tab.id, path: "images/grey_cloud.png"});
-            chrome.pageAction.show(tab.id);
-            console.log("set grey icon");
-        }
-        
-    }, {types: ["main_frame"]}, ["responseHeaders"]);
+chrome.extension.onRequest.addListener(function(request, sender, sendResponse) {
+    
+    if (request.on_cf) {
+        chrome.pageAction.setIcon({tabId: sender.tab.id, path: "images/orange_cloud.png"});
+        chrome.pageAction.show(sender.tab.id);
+    } else {
+        chrome.pageAction.setIcon({tabId: sender.tab.id, path: "images/grey_cloud.png"});
+        chrome.pageAction.show(sender.tab.id);
+    }
+    
+    sendResponse({});   // null response to close the request
+    
 });
 
---
+++ source/contentscript.js
@@ -0,0 +1,18 @@
+// Fetch the current page asynchronously and check the Server header
+// if it matches, send a show page action message
+
+var xhr = new XMLHttpRequest();
+xhr.onreadystatechange = function(data) {
+    if (xhr.readyState == 4) {
+        if (xhr.status == 200) {
+            chrome.extension.sendRequest(
+                {on_cf: (xhr.getResponseHeader("Server") == "cloudflare-nginx")}, 
+                function(response) {}
+            );
+        }
+    }
+}
+
+var url = window.location.href;
+xhr.open('GET', url, true);
+xhr.send();
