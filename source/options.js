d4b9f8f2f65280a06c59e85d9a9105e1a1f0ffe8
Sam Parkinson <sparkinson@outlook.com>
2013-10-07 20:21:18 +0100

    Improved option.js

--- source/options.js
+++ source/options.js
@@ -1,23 +1,26 @@
-var hide_icon_checkbox = document.getElementById('hide_icon_checkbox');
-var debug_log_checkbox = document.getElementById('debug_log_checkbox');
+// list of checkbox id's
+var options =
+[
+	"hide_icon",
+	"debug_logging"
+];
 
-// check current debug state flag and change the checkbox accordingly
-debug_log_checkbox.checked = getOptionValue(localStorage.debug_logging);
+// go through each avaliable option
+for (var i = 0; i < options.length; i++) {
+	(function(option) {
+		var element = document.getElementById(option);
 
-hide_icon_checkbox.checked = getOptionValue(localStorage.hide_icon);
+		// set the value of the checkbox from localStorage
+		element.checked = getOptionValue(option);
 
-// attach a click event to the debug log preference checkbox
-var debug_checkbox_clicked = function(e) {
-    localStorage.debug_logging = (e.target.checked)? 'yes' : 'no';
+		// set the save function when the checkbox is changed
+		element.onclick = function(e) {
+			localStorage.setItem(option, (e.target.checked)? 'yes' : 'no');
+		}
+	})(options[i]);
 };
 
-var hide_icon_clicked = function(e) {
-    localStorage.hide_icon = (e.target.checked)? 'yes' : 'no';
-};
-
-debug_log_checkbox.onclick = debug_checkbox_clicked;
-hide_icon_checkbox.onclick = hide_icon_clicked;
-
-function getOptionValue(setting_value) {
-	return setting_value === 'yes';
+// get a given options value from localStorage
+function getOptionValue(option) {
+	return localStorage.getItem(option) === 'yes';
 }
