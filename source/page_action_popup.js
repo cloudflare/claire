017eba092ecfa72dd3f29ab62e5d09a998f0af7f
Terin Stock <terinjokes@gmail.com>
2015-08-14 01:04:07 -0700

    refactor styles + add copy button

--- source/page_action_popup.js
+++ source/page_action_popup.js
@@ -12,23 +12,35 @@
 		$( '#claireInfoImage' ).hide();
 	}
 
+  $( '.copy-button' ).on('click', function(evt) {
+    var $el = $(this);
+    var copyId = $el.data('copyId');
+    var $copyEl = $('#' + copyId);
+
+    $copyEl.select();
+    document.execCommand('copy');
+
+    evt.preventDefault();
+  });
+
 	chrome.tabs.query( queryInfo, function( tabs ) {
 		var tabID = tabs[0].id;
 		// get the extension's window object
 		var extensionWindow = chrome.extension.getBackgroundPage();
 		var request = extensionWindow.requests[tabID];
 
-		$( '#ip' ).text( request.getServerIP() );
+		$( '#ip' ).val( request.getServerIP() );
 		
 		$( '#claireInfoImage img' ).attr( 'src', request.getPopupPath() );
 
 		// show the Ray ID & location
 		if ( request.servedByCloudFlare() ) {
-			$( '#rayID' ).text( request.getRayID() );
+			$( '#rayID' ).val( request.getRayID() );
 			$( '#locationCode' ).text( request.getCloudFlareLocationCode() );
 			$( '#locationName' ).text( request.getCloudFlareLocationName() );
-			$( '#ray' ).show();
-			$( '#loc' ).show();
+		} else {
+			$( '#ray' ).attr('hidden', true);
+			$( '#loc' ).attr('hidden', true);
 		}
 
 		// show Railgun related info
@@ -39,7 +51,8 @@
 				$( '#railgunCompression' ).text( railgunMetaData.compression );
 				$( '#railgunTime' ).text( railgunMetaData.time );
 			}
-			$( '#railgun' ).show();
+		} else {
+			$( '#railgun' ).attr('hidden', true);
 		}
 	});
 }());
